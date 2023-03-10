e93f0a29884358862ed9913e85622937684a73af
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-03-10 06:06:56 +0100

    End of lecture

--- ConsoleMessages.cs
+++ ConsoleMessages.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 namespace Methods
 {
-	public static class SampleMethods
-	{
+	public static class ConsoleMessages
+    {
 		public static void SayHi(string firstName)
 		{
 			Console.WriteLine($"Hello {firstName}!");
 		}
+
+        public static string GetUsersName()
+        {
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+            return name;
+        }
 
         public static void SayGoodybe()
         {
--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
 ﻿using Methods;
 
-Console.Write("What is your name?: ");
-string name = Console.ReadLine();
+string name = ConsoleMessages.GetUsersName();
 
-SampleMethods.SayHi(name);
+ConsoleMessages.SayHi(name);
 
 
 MathShortcuts.Add(4, 5);
@@ -12,4 +11,4 @@ double[] vals = new double[] { 2, 5, 6, 9, 55, 34, 5 };
 MathShortcuts.AddAll(vals);
 
 
-SampleMethods.SayGoodybe();
+ConsoleMessages.SayGoodybe();
